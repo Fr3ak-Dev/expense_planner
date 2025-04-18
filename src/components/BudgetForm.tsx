ebfa6e7dac219dbd2afa79cc307cf9ea1918f3fa
gustavo <kg.2k.pl@gmail.com>
2025-04-17 23:32:36 -0400

    wrap App component with BudgetProvider

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -15,7 +15,7 @@ export default function BudgetForm() {
     return (
         <form className="space-y-5">
             <div className="flex flex-col space-y-5">
-                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                <label htmlFor="budget" className="text-4xl text-green-600 font-bold text-center">
                     Set Budget
                 </label>
                 <input
