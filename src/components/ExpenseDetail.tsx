e73f25a2e526e6fe0764568df835a5b3f297ea6b
gustavo <kg.2k.pl@gmail.com>
2025-04-20 21:18:18 -0400

    display icons for expense categories

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,24 +1,31 @@
+import { useMemo } from "react"
 import { formatDate } from "../helpers"
 import { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
+import { categories } from "../data/categories"
 
 type ExpenseDetailProps = {
     expense: Expense
 }
 
-export default function ExpenseDetail({expense}: ExpenseDetailProps) {
-  return (
-    <div className="bg-white shadow-lg p-10 w-full border-p border-gray-200 flex gap-5 items-center">
-        <div>
+export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
 
+    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
+    {console.log(categoryInfo.icon)}
+
+    return (
+        <div className="bg-white shadow-lg p-10 w-full border-p border-gray-200 flex gap-5 items-center">
+            <div>
+                <img src={`/icon_${categoryInfo.icon}.svg`} alt="icon" className="w-20" />
+            </div>
+            <div className="flex-1 space-y-2">
+                <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
+                <p>{expense.expenseName}</p>
+                <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+            </div>
+            <AmountDisplay
+                amount={expense.amount}
+            />
         </div>
-        <div>
-            <p>{expense.expenseName}</p>
-            <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
-        </div>
-        <AmountDisplay  
-            amount={expense.amount}
-        />
-    </div>
-  )
+    )
 }
