2a8a35ada788b7d87233dae05b27f2e0d1650e24
gustavo <kg.2k.pl@gmail.com>
2025-04-20 21:42:58 -0400

    Add swipeable actions to ExpenseDetail

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,8 +1,16 @@
 import { useMemo } from "react"
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions
+} from 'react-swipeable-list'
 import { formatDate } from "../helpers"
 import { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
 import { categories } from "../data/categories"
+import "react-swipeable-list/dist/styles.css"
 
 type ExpenseDetailProps = {
     expense: Expense
@@ -11,21 +19,45 @@ type ExpenseDetailProps = {
 export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
 
     const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
-    {console.log(categoryInfo.icon)}
+    { console.log(categoryInfo.icon) }
+
+    const leadingActions = () => (
+        <LeadingActions>
+            <SwipeAction onClick={() => {}}>
+                Share
+            </SwipeAction>
+        </LeadingActions>
+    )
+
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={() => {}} destructive={true}>
+                Delete
+            </SwipeAction>
+        </TrailingActions>
+    )
 
     return (
-        <div className="bg-white shadow-lg p-10 w-full border-p border-gray-200 flex gap-5 items-center">
-            <div>
-                <img src={`/icon_${categoryInfo.icon}.svg`} alt="icon" className="w-20" />
-            </div>
-            <div className="flex-1 space-y-2">
-                <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
-                <p>{expense.expenseName}</p>
-                <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
-            </div>
-            <AmountDisplay
-                amount={expense.amount}
-            />
-        </div>
+        <SwipeableList>
+            <SwipeableListItem
+                maxSwipe={30}   // This is the max distance the user can swipe the item
+                leadingActions={leadingActions()}   // This is the left side actions
+                trailingActions={trailingActions()} // This is the right side actions
+            >
+                <div className="bg-white shadow-lg p-10 w-full border-p border-gray-200 flex gap-5 items-center">
+                    <div>
+                        <img src={`/icon_${categoryInfo.icon}.svg`} alt="icon" className="w-20" />
+                    </div>
+                    <div className="flex-1 space-y-2">
+                        <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
+                        <p>{expense.expenseName}</p>
+                        <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+                    </div>
+                    <AmountDisplay
+                        amount={expense.amount}
+                    />
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
